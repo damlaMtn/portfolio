91065d39dddb0eabb04118e16d6cac931fad6955
Damla Metin <damla91@gmail.com>
2024-01-12 13:39:36 +0300

    visual changes

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -4,27 +4,29 @@ import { Jumbotron } from './migration';
 import {
   Container,
   Row,
+  Col,
 } from "react-bootstrap";
 
 const Experience = ({ experiences }) => {
   return (
     <section className="section">
       <Container>
-        <Jumbotron className="bg-white">
+        <Jumbotron id="experience" className="bg-white">
           <h2 className="display-4 mb-5 text-center">
             {experiences.heading}
           </h2>
           <Row>
-            {
-              experiences.data.map((data, index) => {
-                return <ExperienceCard key={index} data={data} />
-              })
-            }
+            {experiences.data.map((data, index) => (
+              <Col key={index} lg="6">
+                <ExperienceCard data={data} />
+              </Col>
+            ))}
           </Row>
         </Jumbotron>
       </Container>
     </section>
   );
 }
+
 
 export default Experience;
