a6792072ab4f6ee510b8817590150065f50ea4ed
Damla Metin Orak <damla91@gmail.com>
2025-07-14 17:53:39 +0300

    updated resume and surname

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -8,7 +8,7 @@ const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Damla",
   middleName: "",
-  lastName: "Metin Orak",
+  lastName: "Metin",
   message: " .NET Developer | C# | ASP.NET Core",
   icons: [
     {
